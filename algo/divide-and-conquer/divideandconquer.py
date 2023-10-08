ab6717bafe2d7a0e57b89cca8522e9e9acf1d301
Jiayang <jiayang.zhou@outlook.com>
2023-10-08 16:48:51 +0200

    added divide and conquer

---
+++ algo/divide-and-conquer/divideandconquer.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# https://leetcode.com/problems/maximum-binary-tree/
+def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        def build(arr):
+            arrmax = max(arr)
+            index = arr.index(arrmax)
+            node = TreeNode(arrmax)
+            if arr[:index]:
+                node.left = build(arr[:index])
+            if arr[index + 1:]:
+                node.right = build(arr[index + 1:])  
+            return node
+        return build(nums)
