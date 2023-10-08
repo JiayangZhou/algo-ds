b1beca4fa45ad5c9c522d8e691c79ccce91f79df
Jiayang <jiayang.zhou@outlook.com>
2023-10-08 14:06:43 +0200

    added bt

--- algo/backtracking/backtracking.py
+++ algo/backtracking/backtracking.py
@@ -15,7 +15,23 @@ def restoreIpAddresses(s: str) -> List[str]:
             bt(index + 1, l + [last] + [s[index]])
         bt(1, [s[0]])
         return res
-restoreIpAddresses("25525511135")
+# restoreIpAddresses("25525511135")
 
+# https://leetcode.com/problems/word-break-ii/
+def wordBreak(s: str, wd: List[str]) -> List[str]:
+    res = []
+    n = len(s)
+    def bt(index, arr):
+        if index >= n:
+            if arr[-1] in wd:
+                res.append(" ".join(arr))
+            return
+        last = arr.pop()
+        if last in wd:
+            bt(index + 1, arr + [last] + [s[index]])
+        bt(index + 1, arr + [last + s[index]])
 
+    bt(1, [s[0]])
+    return list(set(res))
+wordBreak(s="pineapplepenapple", wd=["apple","pen","applepen","pine","pineapple"])
 
