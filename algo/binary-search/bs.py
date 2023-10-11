8f36957fb42e43cf51afde420550ab384d504e36
Jiayang <jiayang.zhou@outlook.com>
2023-10-11 21:05:45 +0200

    added bs

--- algo/binary-search/bs.py
+++ algo/binary-search/bs.py
@@ -29,3 +29,27 @@ def shipWithinDays(weights: list[int], days: int) -> int:
 # shipWithinDays([1,2,3,1,1], 4) # 3
 # shipWithinDays([3,2,2,4,1,4], 3) # 6
 
+# https://leetcode.com/problems/random-pick-with-weight/
+class Solution:
+    def __init__(self, w: List[int]):
+        self.arr = []
+        self.cur = 0
+        for _ in w:
+            self.cur += _
+            self.arr.append(self.cur)
+        
+    def pickIndex(self) -> int:
+        import random
+        r = random.randint(1, self.cur)
+        left = 0
+        right = len(self.arr) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if self.arr[mid] < r:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+# obj = Solution([3,14,1,7])
+# param_1 = obj.pickIndex()
+# print(param_1)
