6e3799d70d726b996bc7215c264bed4275b629b4
Jiayang <jiayang.zhou@outlook.com>
2023-10-12 12:11:50 +0200

    added dll

---
+++ ds/dll.py
@@ -0,0 +1,34 @@
+# double linked list
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.freq = 1
+        self.prev = None
+        self.next = None
+        
+class DLL:
+    def __init__(self):
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+        
+    def insertHead(self, node):
+        headNext = self.head.next
+        headNext.prev = node
+        self.head.next = node
+        node.prev = self.head
+        node.next = headNext
+        self.size += 1
+        
+    def removeNode(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        self.size -= 1
+        
+    def removeTail(self):
+        tail = self.tail.prev
+        self.removeNode(tail)
+        return tail
