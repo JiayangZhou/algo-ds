129e17d270c7cfef192918f0d8246b9128f53d19
Jiayang <jiayang.zhou@outlook.com>
2023-10-13 14:23:01 +0200

    added a straight forward dp

--- algo/dp/bottom-up.py
+++ algo/dp/bottom-up.py
@@ -23,3 +23,10 @@ def maximizeTheProfit(n: int, offers: List[List[int]]) -> int:
         dp[i] = dp[i - 1] if dp[i] < dp[i - 1] else dp[i]
     return dp[n - 1]
 
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+def minCostClimbingStairs(self, cost: List[int]) -> int:
+    n = len(cost)
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+    return dp[n]
