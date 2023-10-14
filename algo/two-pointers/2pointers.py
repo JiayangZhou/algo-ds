0437309c77bbbe4696a3c0c76b39eaea1c316954
Jiayang <jiayang.zhou@outlook.com>
2023-10-14 11:46:55 +0200

    added 3sum

--- algo/two-pointers/2pointers.py
+++ algo/two-pointers/2pointers.py
@@ -18,3 +18,26 @@ def maxArea(h: List[int]) -> int:
         else:
             right -= 1
     return res
+
+# https://leetcode.com/problems/3sum/
+# 2sum can also use two pointers, just need to sort array first
+def threeSum(nums: List[int]) -> List[List[int]]:
+    n = len(nums)
+    nums.sort()
+    ans = []
+    for i in range(n):
+        t = -nums[i]
+        l = i + 1
+        r = n - 1
+        while l < r:
+            if nums[l] + nums[r] == t:
+                _ = sorted([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+                if _ not in ans:
+                    ans.append(_)
+            elif nums[l] + nums[r] > t:
+                r -= 1
+            else:
+                l += 1
+    return ans
