a769de2f6d2017951e1f65b530a6550b5b162dea
Jiayang <jiayang.zhou@outlook.com>
2023-10-15 17:04:05 +0200

    added another interval question

--- algo/sorting/intervals.py
+++ algo/sorting/intervals.py
@@ -1,3 +1,4 @@
+from typing import List
 # https://leetcode.com/problems/merge-intervals
 def merge(self, a: List[List[int]]) -> List[List[int]]:
     ans = []
@@ -15,3 +16,29 @@ def merge(self, a: List[List[int]]) -> List[List[int]]:
             r = max(cur[1], r)
     ans.append([l, r])
     return ans
+
+# https://aaronice.gitbook.io/lintcode/sweep-line/meeting-rooms-ii
+from heapq import heapify, heappop, heappush
+def meetingRoom(a):
+    s = []
+    e = []
+    for _ in a:
+        s.append(_[0])
+        e.append(_[1])
+    heapify(s)
+    heapify(e)
+    rooms = 0
+    free = 0
+    while s and e:
+        if s[0] < e[0]:
+            heappop(s)
+            if free > 0:
+                free -= 1
+            else:
+                rooms += 1
+        else:
+            heappop(e)
+            free += 1
+    return rooms
+    
+meetingRoom([[0, 30],[5, 10],[15, 20]])
