c24e415fc9b4f4bd14d9555a1e520da4a1fd7d4a
Jiayang <jiayang.zhou@outlook.com>
2023-10-28 21:50:14 +0200

    added LFU

--- ds/dll.py
+++ ds/dll.py
@@ -33,4 +33,41 @@ class DLL:
         self.removeNode(tail)
         return tail
     
-# LFU
+# LFU, need more practice
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.cache = {}
+        from collections import defaultdict
+        self.freqTable = defaultdict(DLL)
+        self.capacity = capacity
+        self.minFreq = 0
+        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        return self.updateCache(self.cache[key], key, self.cache[key].val)
+        
+    def put(self, key: int, value: int) -> None:
+        if not self.capacity:
+            return
+        if key in self.cache:
+            self.updateCache(self.cache[key], key, value)
+        else:
+            if len(self.cache) == self.capacity:
+                prevTail = self.freqTable[self.minFreq].removeTail()
+                del self.cache[prevTail.key]
+            node = ListNode(key, value)
+            self.freqTable[1].insertHead(node)
+            self.cache[key] = node
+            self.minFreq = 1
+        
+    def updateCache(self, node, key, value):
+        node = self.cache[key]
+        node.val = value
+        prevFreq = node.freq
+        node.freq += 1
+        self.freqTable[prevFreq].removeNode(node)
+        self.freqTable[node.freq].insertHead(node)
+        if prevFreq == self.minFreq and self.freqTable[prevFreq].size == 0:
+            self.minFreq += 1
+        return node.val
