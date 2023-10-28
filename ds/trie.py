854637216cdc4cbf3fce717471b5cdc4b30634de
Jiayang <jiayang.zhou@outlook.com>
2023-10-28 13:18:59 +0200

    added trie implementation

--- ds/trie.py
+++ ds/trie.py
@@ -1,3 +1,4 @@
+from typing import List
 # https://leetcode.com/problems/implement-trie-prefix-tree/
 class TrieNode:
     def __init__(self):
@@ -33,3 +34,18 @@ class Trie:
                 return False
             cur = cur.children[c]
         return True
+    
+# https://leetcode.com/problems/length-of-the-longest-valid-substring
+# Trie implementation
+def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
+    t = Trie()
+    for _ in forbidden:
+        t.insert(_)
+    ans = 0
+    start = 0 
+    for i in range(len(word)):
+        for j in range(max(start, i - 9), i + 1):
+            if t.search(word[j:i + 1]):
+                start = j + 1
+        ans = max(ans, i + 1 - start)
+    return ans
