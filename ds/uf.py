6f901b654c5bd761f0c3a37ede20c4db9728d28a
Jiayang <jiayang.zhou@outlook.com>
2023-11-02 19:21:44 +0100

    added UF

--- ds/uf.py
+++ ds/uf.py
@@ -12,3 +12,21 @@ def union(x, y):
         UF[xRoot] = yRoot
     else:
         UF[yRoot] = xRoot
+        
+# another implementation
+def find(x):
+    if x not in UF:
+        UF[x] = x
+        return x
+    else:
+        if UF[x] == x:
+            return x
+        return find(UF[x])
+
+def union(x, y):
+    xRoot = find(x) 
+    yRoot = find(y)
+    if xRoot < yRoot:
+        UF[yRoot] = UF[xRoot]
+    else:
+        UF[xRoot] = UF[yRoot]
