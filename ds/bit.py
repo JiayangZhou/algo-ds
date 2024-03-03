9d4d5c3d2ccfe01faac98fa7038a91297641d6cf
Jiayang <jiayang.zhou@outlook.com>
2024-03-03 14:17:20 +0100

    bitwise AND

--- ds/bit.py
+++ ds/bit.py
@@ -1,6 +1,15 @@
+# bitwise and
+print(1 & 1) # 1
+print(0 & 1) # 0
+print(5 & 4) # 4, the result of x & (x - 1) is x removed rightmost significant number, any number that is power of 2, & its minus one, would result in 0
+print(1024 & 1023) # 0, this is an example of above
+
+# bitwise exclusive or
 print(1 ^ 1) # 0
 print(1 ^ 0) # 1
 print(0 ^ 0) # 0
+
+# bitwise or
 print(1 | 0) # 1 
 print(1 | 1) # 1
 
